f41d02f138c99f01c4f322db22bcceb64cb8196d
Nathan Preslar <nathanpreslar@Nathans-MacBook-Pro-35.local>
2026-01-20 22:44:28 -0700

    Added reversal and momentum signal flows

--- pipelines/signal_flows/momentum_flow.py
+++ pipelines/signal_flows/momentum_flow.py
@@ -1,19 +1,51 @@
 import datetime as dt
 from pipelines.utils.tables import Database
 import polars as pl
+from pipelines.signal_flows.reversal_flow import get_assets_signal
 
+def momentum_backfill_flow(
+    start_date: dt.date, 
+    end_date: dt.date, 
+    database: Database
+) -> None:
+    # get signal
+    signal = get_signal("momentum")
 
-def momentum_backfill_flow(start_date: dt.date, end_date: dt.date, database: Database) -> None:
-    # This is all just nonsense - Andrew
-    # get data
-    # - get factor exposures
-    # - run pca
-    # compute signal
     for year in range(start_date.year, end_date.year + 1):
-        year_df = pl.DataFrame().filter(pl.col('date').year.eq(year))
+        # get asset data to compute signal for a given year
+        year_data = get_assets_signal(
+            start_date=(dt.date(year, 1, 1) - dt.timedelta(days=signal.lookback_days)),
+            end_date=dt.date(year, 12, 31),
+            signal_name=signal.name
+        )
+        print(f"\nAsset data for {year}:\n")
 
+        # compute signal, filter to year, select relevant columns
+        year_data = (
+            year_data
+            .with_columns(signal.expr)
+            .with_columns(
+                pl.lit(signal.name).alias("signal_name")
+            )
+            .select(["date", "barrid", "ticker", "signal_name", f"{signal.name}"])
+            .rename({f"{signal.name}": "signal_value"})
+        )
+        signals_df_year = year_data.filter(
+            (pl.col("date").dt.year() == year)
+        )
+        print(f"\nSignals dataframe:\n{signals_df_year}")
+
+        # upsert into database
         database.signals_table.create_if_not_exists(year)
         database.signals_table.upsert(year, rows=year_df)
 
 
-    pass
+if __name__ == "__main__":
+    database_name = DatabaseName("production")
+    database_instance = Database(database_name)
+
+    momentum_backfill_flow(
+        start_date=dt.date(2023, 1, 1),
+        end_date=dt.date(2025, 12, 31),
+        database=database_instance
+    )
--- pipelines/signal_flows/reversal_flow.py
+++ pipelines/signal_flows/reversal_flow.py
@@ -4,13 +4,15 @@ import polars as pl
 from pipelines.components.signals import get_signal
 import sf_quant.data as sfd
 from pipelines.utils.enums import DatabaseName
-# import dataframely as dy
+import dataframely as dy
+
 
 def get_assets_signal(
     start_date: dt.date,
     end_date: dt.date,
     signal_name: str
 ) -> pl.DataFrame:
+    """"""
     signal = get_signal(signal_name)
     lookback_days = signal.lookback_days
 
@@ -25,13 +27,12 @@ def get_assets_signal(
 
     asset_data = (
         sfd.load_assets(
-            start=start_date - dt.timedelta(days=lookback_days),
+            start=(start_date - dt.timedelta(days=lookback_days * 2)),
             end=end_date, 
             columns=columns, 
             in_universe=True
         )
         .with_columns(pl.col("return").truediv(100))
-        # .filter(pl.col("ticker").is_in(tickers))
         .sort("ticker", "date")
     )
     return asset_data
@@ -44,29 +45,51 @@ def reversal_backfill_flow(
     end_date: dt.date, 
     database: Database
 ) -> None:
-
     # get signal
     signal = get_signal("reversal")
     
-    # iteratively get data, compute signal, save to database
     for year in range(start_date.year, end_date.year + 1):
+        # get asset data to compute signal for a given year
         year_data = get_assets_signal(
-            start_date=start_date,
-            end_date=end_date,
+            start_date=dt.date(year, 1, 1),
+            end_date=dt.date(year, 12, 31),
             signal_name=signal.name
         )
+        print(f"\nAsset data for {year}:\n")
 
-        year_data = year_data.with_columns(
-            signal.expr.alias(signal.name),
-            pl.lit(signal.name).alias("signal_name")
+        # compute signal, filter to year, select relevant columns
+        year_data = (
+            year_data
+            .with_columns(signal.expr)
+            .with_columns(
+                pl.lit(signal.name).alias("signal_name")
+            )
+            .select(["date", "barrid", "ticker", "signal_name", f"{signal.name}"])
+            .rename({f"{signal.name}": "signal_value"})
         )
-
+        print(f"\nyear data after computing signal:\n{year_data}")
         signals_df_year = year_data.filter(
-            (pl.col("date").dt.year == year)
+            (pl.col("date").dt.year() == year)
         )
-
+        print(f"\nSignals DataFrame for {year}:\n{signals_df_year}")
+        
+        # upsert into database
         database.signals_table.create_if_not_exists(year)
         database.signals_table.upsert(year, rows=signals_df_year)
+
+
+
+# def reversal_signal_daily_flow(
+#     database: Database
+# ) -> None:
+#     # get signal
+#     signal = get_signal("reversal")
+
+#     # get the last day of the signal
+#     last_signal_date = database.signals_table.get_last_date(signal.name)
+#     return
+    
+
 
 if __name__ == "__main__":
     database_name = DatabaseName("production")
@@ -74,6 +97,6 @@ if __name__ == "__main__":
 
     reversal_backfill_flow(
         start_date=dt.date(2023, 1, 1),
-        end_date=dt.date(2024, 1, 1),
+        end_date=dt.date(2025, 12, 31),
         database=database_instance
     )
